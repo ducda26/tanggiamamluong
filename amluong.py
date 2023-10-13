ad7f94f44d435308f24e005634197c9aa3bcaa4f
ducda26 <doananhduc26@gmail.com>
2023-10-13 14:37:46 +0700

    Phat hien ban tay

--- amluong.py
+++ amluong.py
@@ -1,11 +1,21 @@
 import cv2
 import time 
+import hand as htm
 
 pTime=0 #Thời gian bắt đầu
 cap = cv2.VideoCapture(0)
+detector = htm.handDetector(detectionCon=0.7) 
+#detectionCon=0.7 độ tin cậy phát hiện ra lòng bàn tay là 0.7
 
 while True:
     ret, frame = cap.read()
+    frame = detector.findHands(frame) #Tìm bàn tay (phát hiện)
+    lmList = detector.findPosition(frame, draw=False) # phát hiện vị trí, đẩy vào list các vị trí
+    # print(lmList) đẩy ra vị trí của 21 point (từ 0-20)  trên bàn tay
+    
+    if len(lmList)!= 0: #Có tín hiệu bàn tay mới bắt đầu làm
+        # cần sử dụng 2 ngón trỏ và ngón cái (point 4, và 8)
+        print(lmList[4],lmList[8]) # đẩy về giá trị điểm 4 và 8
     
     # viết ra FPS
     cTime = time.time()  # trả về số giây, tính từ 0:0:00 ngày 1/1/1970 theo giờ  utc , gọi là(thời điểm bắt đầu thời gian)
